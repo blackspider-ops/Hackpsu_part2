89a1bc45e0f692eaab9927656c68a9b67e890b5f
devank-yadav <devank22yadav@gmail.com>
2025-03-29 17:37:43 -0400

    Final Commit

--- app/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-# Import routes and possibly forms here to register them with the blueprint
-from . import routes # noqa: F401 E402
--- app/users/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-users = Blueprint('users', __name__)
-
-from . import routes # noqa: F401 E402
